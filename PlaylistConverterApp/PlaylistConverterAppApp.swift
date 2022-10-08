0e8393b9b1db8c84517e0cede5d9806dd182ec27
Tomasz Wojtyniak <tomasz.w@moodup.team>
2022-10-08 12:21:14 +0200

    Initial Commit

---
+++ PlaylistConverterApp/PlaylistConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylistConverterAppApp.swift
+//  PlaylistConverterApp
+//
+//  Created by Tomasz Wojtyniak on 08/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PlaylistConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
